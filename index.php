ccd5db845b4ae6e7aa8f81d6b13be12818321fb4
yulchitaj <ys@jelastic.com>
2015-01-09 14:37:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 if (version_compare(PHP_VERSION, '5.3.10', '<'))
 {
-	die('Your host needs to use PHP 5.3.10 or higher to run this version of Joomla!');
+	die('Your host needs to use PHP 5.3.10 or higher to run this version of Joomla!Blablabla');
 }
 
 /**
